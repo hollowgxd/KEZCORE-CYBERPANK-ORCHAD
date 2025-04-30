49883164d4c7f8d74dd1805374e390bb4d0a4e32
hollowgxd <devdplayer@yandex.ru>
2025-04-30 12:52:18 +0300

    added animations

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,30 @@
 const config = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // обязательно!
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00FF00',
+        secondary: '#FF00FF',
+      },
+      fontFamily: {
+        mono: ['"Courier New"', 'monospace'],
+      },
+      backgroundImage: {
+        'cyber-gradient': 'linear-gradient(to right, #00FF00, #FF00FF)',
+      },
+      animation: {
+        'pulse-glow': 'pulseGlow 1.5s ease-in-out infinite',
+      },
+      keyframes: {
+        pulseGlow: {
+          '0%, 100%': { boxShadow: '0 0 10px #00ff7f' },
+          '50%': { boxShadow: '0 0 20px #39ff14, 0 0 30px #39ff14' },
+        },
+      },
+    },
+  },
   plugins: ["@tailwindcss/postcss"],
 };
 
--- src/app/components/WorkerInfo.tsx
+++ src/app/components/WorkerInfo.tsx
@@ -1,7 +1,7 @@
 // src/components/WorkerInfo.tsx
 const WorkerInfo = () => {
   return (
-    <div className="bg-black text-green-400 p-6 rounded-lg border-2 border-green-600">
+    <div className="bg-black text-green-400 p-6 rounded-lg border-2 border-green-600 animate-pulse-glow">
       <h2 className="text-3xl font-bold mb-4">Сведения о Работниках</h2>
       <p className="text-gray-300">Информация о текущем обслуживании куриц и яйценоскости.</p>
     </div>
